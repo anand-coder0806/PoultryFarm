14967f10f4017e921aef4ac41780eb1edd0b5fbb
Anand Bhagat <ab471448@gmail.com>
2026-06-16 14:46:29 +0530

    letestchange

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require('vite');
+
+module.exports = defineConfig({
+  server: {
+    host: '0.0.0.0',
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
